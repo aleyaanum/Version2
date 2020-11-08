5147eb20bd6784bca8a31c7dc02a571b1e3bcbb6
aleyaanum <53285932+aleyaanum@users.noreply.github.com>
2020-11-08 16:52:29 +0800

    php_timbanganversion2.php

---
+++ php_timbangan.php
@@ -0,0 +1,26 @@
+<html>
+<title> PHP-Timbangan</title>
+
+<body>
+	<form action="PHP-Timbangan.php" method="POST">
+		Masukkan berat anda <input type="TEXT" name="nilai" value="0" />
+		<form>
+
+<?php
+if (POST [nilai1] >= 80)
+{
+	$result = "Anda Berat"
+
+}
+ELSE
+{
+	$result = "Berat Anda Adalah Normal";
+}
+
+>
+mesej untuk Anda :
+<?PHP
+ECHO "$result";
+?>
+</body>
+</html>
